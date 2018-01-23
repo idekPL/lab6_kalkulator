5997b50b42caa72d39f4ac1cbfada3ebb49ae1de
idekPL <35067366+idekPL@users.noreply.github.com>
2018-01-23 18:11:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,13 +9,40 @@
 using namespace std;
 
 double a, b;
-
+int wybor;
 int main()
 {
+cout<<"Wybierz działanie:"<<endl;
+cout<<"1 - Dowadanie; 2 - Odejmowanie; 3 - Mnożenie; 4 - Dzielenie"<<endl;
+cin>>wybor;
 cout<<"Podaj a: ";
 cin>>a;
 cout<<"Podaj b: ";
 cin>>b;
 
-return 1;
+switch(wybor)
+{
+  case 1:
+    {
+    cout<<dodawanie(a, b);
+    break;  
+    }
+  case 2:
+    {
+    cout<<odejmowanie(a,b);
+    break;  
+    }  
+  case 3:
+    {
+    cout<<mnozenie(a, b);
+    break;
+    }
+  case 4:
+    {
+    cout<<dzielenie(a, b);
+    break;  
+    }    
+}    
+  
+return 0;
 }
